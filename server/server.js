f584acea35b607ef380efaff2114e9818ab6e368
Kristopher Cabado <cabadokris32@gmail.com>
2023-10-27 14:18:31 -0700

    "fixed serverjs"

--- server/server.js
+++ server/server.js
@@ -6,13 +6,8 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-// app.use(cors({
-//     origin: "http://localhost:3000"
-// }));
-
 app.use(cors({
-    origin: ["https://taskodoro-backend-77z1qb2rp-cabadokris32-gmailcom.vercel.app/"],
-    methods: ["POST", "GET"]
+    origin: "http://localhost:3000"
 }));
 
 require("./config/mongoose.config");
