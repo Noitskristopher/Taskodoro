ff83abd2edfdb723fac651e12056b62cfb78e77d
Kristopher Cabado <cabadokris32@gmail.com>
2023-11-06 13:48:27 -0800

    added login feature

--- server/controllers/user.controller.js
+++ server/controllers/user.controller.js
@@ -26,5 +26,25 @@ module.exports = {
             console.log(err);
             res.status(400).json({ error: err })
         }
+    },
+    loginUser: async (req, res) => {
+        try {
+            const user = await User.findOne({ email: req.body.email });
+            if (user) {
+                const passwordMatch = await bcrypt.compare(req.body.password, user.password)
+                if (passwordMatch) {
+                    const userToken = jwt.sign({ _id: user._id, email: user.email }, secret, { expiresIn: '2h' })
+                    console.log(userToken)
+                    res.cookie('userToken', userToken, { httpOnly: true, maxAge: 2 * 60 * 60 * 1000 }).status(201).json({ message: "User logged in", user: user })
+                } else {
+                    res.status(400).json({ message: 'Invalid Email/Password' })
+                }
+            } else {
+                res.status(400).json({ message: 'Invalid Email/Password' })
+            }
+        }
+        catch (err) {
+            res.status(400).json({ message: 'err' })
+        }
     }
 }
